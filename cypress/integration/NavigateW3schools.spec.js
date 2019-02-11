ef99c5e28fa535af1c3449b63a96a13ee5aa5ebf
SOUsss <43376585+SOUsss@users.noreply.github.com>
2019-02-11 15:05:49 +0200

    test

---
+++ cypress/integration/NavigateW3schools.spec.js
@@ -0,0 +1,8 @@
+describe('sample',()=>{
+    it("signup with value", () => {
+ cy.visit('https://www.w3schools.com/w3css/')
+ cy.get('.w3-bar > [href="/sql/default.asp"]').click()
+
+
+    })
+})
---
+++ cypress/integration/SignupBrowrerstack.spec.js
@@ -0,0 +1,19 @@
+describe('signup',()=>{
+  it('login with values', () => {
+    cy.visit("https://www.browserstack.com/users/sign_up")
+  })
+
+ 
+  it('Test login with values', () => {
+    cy.get('#user_email_login').type('Fra79k@gmail.com');
+    cy.get('#user_password').type('TestP5Bswo3rd');
+    cy.get('#user_full_name').type('Test Test');
+    cy.get('#user_group_attributes_name').type('432235 ');
+    cy.get('p > input').click();
+    cy.get('[style="width: 304px; height: 78px;"] > div > iframe').click();
+    cy.get('#user_submit').click()
+    //.should("https://accounts.browserstack.com/confirmation?code=")
+      
+  })
+
+})
