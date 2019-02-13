0d2bed194c33abda2ed1abaad7da1a5396b6583e
SOUsss <43376585+SOUsss@users.noreply.github.com>
2019-02-13 20:04:55 +0200

    Added expected result to tests.

--- cypress/integration/NavigateW3schools.spec.js
+++ cypress/integration/NavigateW3schools.spec.js
@@ -2,5 +2,6 @@ describe('sample',()=>{
     it("signup with value", () => {
      cy.visit('https://www.w3schools.com/w3css/')
      cy.get('.w3-bar > [href="/sql/default.asp"]').click()
+     cy.get('#main > h1').should('have.text','SQL Tutorial')
     })
 })
--- cypress/integration/SignupFB.spec.js
+++ cypress/integration/SignupFB.spec.js
@@ -5,12 +5,12 @@ describe('signup', () => {
     it('Signup with values', () => {
     cy.get('#u_0_j').type('Jack') 
     cy.get('#u_0_l').type('Jhonson')    
-    cy.get('#u_0_o').type("Tehv434g@gmail.com")
-    cy.get('#u_0_r').type("Tehv434g@gmail.com")
-    cy.get('#u_0_v').type("TesT23tTrw") 
+    cy.get('#u_0_o').type('Tehv434g@gmail.com')
+    cy.get('#u_0_r').type('Tehv434g@gmail.com')
+    cy.get('#u_0_v').type('TesT23tTrw') 
     cy.get('#u_0_a').click()
     cy.get('#u_0_11').click()
-    cy.get('.layerConfirm').click()
+    cy.get('#u_0_5').should('have.text','Підтвердьте свій день народження')
   })
    
 })   
